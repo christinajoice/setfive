aeb4de0d24c3ab2de370f87d09b5257566d02851
christinajoice <christinallrejoice@gmail.com>
2019-02-01 16:10:25 +0530

    Create PrintK.java

---
+++ PrintK.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class PrintK
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+Systm.out.println("Enter a string");
+String str=s.nextLine();
+System.out.println("Enter K value");
+int k=s.nextInt();
+for(int i=0;i<k;i++)
+[
+System.out.println(str);
+}
+}
+}
