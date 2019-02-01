8634056e04a909d84c9b04a8d3efbee8cf1f5c7b
christinajoice <christinallrejoice@gmail.com>
2019-02-01 08:56:25 -0800

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,1 +1,14 @@
 import java.io.*;
+import java.util.*;
+public class Sum
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+  System.out.println("Enter two numbers");
+  int n1=s.nextInt();
+  int n2=s.nextInt();
+  int sum=n1+n2;
+  System.out.println(sum);
+}
+}
