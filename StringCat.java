dea023d1d5574fd23f4e4f41c0aacd31349b9aba
christinajoice <christinallrejoice@gmail.com>
2019-02-01 16:29:01 +0530

    Create StringCat.java

---
+++ StringCat.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class StringCat
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter two strings");
+String str1=s.nextLine();
+String str2=s.nextLine();
+System.out.println(str1+str2);
+}
+}
